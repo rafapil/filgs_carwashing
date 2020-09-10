e9fb9c4e8b47adc27ce3090d5b3f5758341fe9e3
rafasera <rafael.filgue@gmail.com>
2020-09-09 23:06:34 -0300

    trabalhando no cadastro cliente

--- lib/ui/cadastro_cliente.dart
+++ lib/ui/cadastro_cliente.dart
@@ -6,18 +6,53 @@ class CadastroClientes extends StatefulWidget {
 }
 
 class _CadastroClientesState extends State<CadastroClientes> {
+  final _nomeController = TextEditingController();
+  final _telefoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final _screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Cadastro de Clientes'),
-            ],
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: _screenHeight / 9,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Cadastro de Clientes',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.w800),
+                ),
+              ],
+            ),
+            Text('Nome'),
+            TextField(
+              keyboardType: TextInputType.name,
+              controller: _nomeController,
+              decoration: InputDecoration(hintText: 'Nome do Cliente'),
+            ),
+            Text('Telefone'),
+            TextField(
+              keyboardType: TextInputType.phone,
+              controller: _telefoneController,
+              decoration: InputDecoration(hintText: 'ddd + numero'),
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                child: Text('Salvar'),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
